778dc9e0041e1c6d42204b948ff8d0e26c1d078a
Aathif <aathif.b@mstsoft.co>
2025-05-14 16:08:43 +0530

    login auth added

--- utils/ringcentral_auth.py
+++ utils/ringcentral_auth.py
@@ -1,13 +1,19 @@
 import os
+import json
 from dotenv import load_dotenv
 from ringcentral import SDK
 
+print("Starting RingCentral JWT auth...")
+
 load_dotenv()
 
-sdk = SDK(os.getenv("RC_CLIENT_ID"), os.getenv("RC_CLIENT_SECRET"), os.getenv("RC_SERVER_URL"))
+RC_CLIENT_ID = os.getenv("RC_CLIENT_ID")
+RC_CLIENT_SECRET = os.getenv("RC_CLIENT_SECRET")
+RC_SERVER_URL = os.getenv("RC_SERVER_URL")
+RC_JWT = os.getenv("RC_JWT")
+sdk = SDK(RC_CLIENT_ID, RC_CLIENT_SECRET, RC_SERVER_URL)
 
 platform = sdk.platform()
-platform.login(jwt=os.getenv("RC_JWT"))
 
 def register_webhook():
     url = "https://voiceagent-0wtp.onrender.com/ringcentral/webhook"
@@ -23,3 +29,27 @@ def register_webhook():
     }
     response = platform.post("/restapi/v1.0/subscription", body)
     print("Webhook Registered", response.json())
+
+
+# Perform Jwt login
+try: 
+    platform.login(
+        jwt=RC_JWT
+    )
+    print("Login Successfully!")
+
+    # Get token data
+    token = platform.auth().data()
+
+    # Print Token
+    print(json.dumps(token, indent=2))
+
+    # Save token to file
+    with open(".token.json", "w") as f:
+        json.dump(token, f)
+    print("Token save to file")
+
+    register_webhook()
+
+except Exception as e:
+    print("Login failed", str(e))
