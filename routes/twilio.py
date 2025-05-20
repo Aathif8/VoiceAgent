3fbff9f35291beb860692e7dd0adf920ba0df899
Aathif <aathif.b@mstsoft.co>
2025-05-20 12:02:25 +0530

    Timeout updated

--- routes/twilio.py
+++ routes/twilio.py
@@ -21,7 +21,7 @@ async def twilio_answer(request: Request):
         method="POST",
         max_length=30,
         play_beep=True,
-        timeout=1
+        timeout=5
     )
     response.say("No input received. GoodBye.")
     response.hangup()
