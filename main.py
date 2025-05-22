5b893ecc5f9623475054f22d4c31bcd75893817b
Aathif <aathif.b@mstsoft.co>
2025-05-22 12:14:00 +0530

    Websocket parameter Changed

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ async def handle_media_stream(websocket: WebSocket):
 
     async with websockets.connect(
        'wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01',
-       extra_headers=headers
+       headers=headers,
     ) as openai_ws:
         await send_session_update(openai_ws)
         stream_sid = None
