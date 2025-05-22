58722243a8c4f0952799774c9a42aae8096867d6
Aathif <aathif.b@mstsoft.co>
2025-05-22 12:08:28 +0530

    Websocket header Updated

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.websockets import WebSocketDisconnect
-from twilio.twiml.voice_response import VoiceResponse, Connect, Say, Stream
+from twilio.twiml.voice_response import VoiceResponse, Connect
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -45,6 +45,11 @@ LOG_EVENT_TYPES = [
     'response.content.done', 'rate_limitts.updated', 'response.done', 'input_audio_buffer.committed', 'input_audio_buffer.speech_stopped', 'input_audio_buffer.speech_started', 'session.created'
 ]
 
+headers = [
+    ("Authorization", f"Bearer {OPENAI_API_KEY}"),
+    ("OpenAI-Beta", "realtime=v1")
+]
+
 app = FastAPI()
 
 if not OPENAI_API_KEY:
@@ -72,10 +77,7 @@ async def handle_media_stream(websocket: WebSocket):
 
     async with websockets.connect(
        'wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01',
-       extra_headers={
-           "Authorization": f"Bearer {OPENAI_API_KEY}",
-           "OpenAI-Beta": "relatime=v1"
-       } 
+       extra_headers=headers
     ) as openai_ws:
         await send_session_update(openai_ws)
         stream_sid = None
